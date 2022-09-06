2d280ae1c140ab5a4a3b6a74f7c27d1c00f561a3
Dastan Kadylbekov <dastan.962601@gmail.com>
2022-09-06 21:38:29 +0600

    add home tasks

---
+++ index.js
@@ -0,0 +1,29 @@
+const a = 10;
+const b = 2;
+
+console.log(a + b, a - b, a * b, a / b);
+
+// const c = 15;
+// const d = 2;
+
+// console.log(result = (c + d));
+
+// const a = 17;
+// const b = 10;
+
+// let c = (a - b);
+
+// let d = 7;
+
+// console.log(result = c + d);
+
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+
+// alert(Number(a) + Number(b));
+
+
+
+
